ddde8132ca9afe98cd1a06f328a913aa1e00ef25
Lucivan Max <lucivanmax@gmail.com>
2026-04-26 18:37:24 +0000

    Criandos as Variáveis das 2 Cartas.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,24 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  /* Declarando variáveis da carta1*/
+  char estado1;
+  char codigo1[5];
+  char cidade1[50];
+  int populacao1;
+  float area1;
+  float pib1;
+  int pontosTuristicos1;
+
+
+  /* Declarando variáveis da carta2*/
+  char estado2;
+  char codigo2[5];
+  char cidade2[50];
+  int populacao2;
+  float area2;
+  float pib2;
+  int pontosTuristicos2;
 
   // Área para entrada de dados
 
